b050518fd0a9b144a0499a856f8c3b89b8fb3213
laamyry <slamiri10@gmail.com>
2023-09-22 23:59:06 +0100

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,11 @@
 #create a file in /tmp.
 
 file { '/tmp/school':
-  ensure     => 'file',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet'
+  path		=
+  ensure	=> 'file',
+  mode		=> '0744',
+  owner		=> 'www-data',
+  group		=> 'www-data',
+  content	=> 'I love Puppet'
  }
  
