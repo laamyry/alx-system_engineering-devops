75dc7ef1a907ff478122a4662ffbe38284557787
laamyry <slamiri10@gmail.com>
2023-09-24 02:26:38 +0100

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 #changes to our configuration file
 
-file { 'ect/ssh/ssh_config':
-  ensure  => present,
+file1 { 'ect/ssh/ssh_config':
   path    => 'ect/ssh/ssh_config',
-  centent => "
-    #SSH configuration
-    Host 
-       PasswordAuthentication no
-       IdentityFile ~/.ssh/school
-",
+  content => 'PasswordAuthentication no',
+}
+file2 { 'ect/ssh/ssh_config':
+  path    => 'ect/ssh/ssh_config',
+  content => 'IdentityFile ~/.ssh/school',
 }
