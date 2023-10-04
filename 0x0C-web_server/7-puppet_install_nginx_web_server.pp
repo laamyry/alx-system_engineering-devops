38a1e12ed078bfb203f407f8d985fc56d3553124
laamyry <slamiri10@gmail.com>
2023-10-04 02:33:08 +0100

    0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 #Install Nginx web server (w/ Puppet)
 
-exec {'install nginx':
+exec {'install':
   provider => shell,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
   echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ;
