14784e1b50476aa406878330a450d09f2808365f
laamyry <slamiri10@gmail.com>
2023-10-04 01:46:14 +0100

    0x0C-web_server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+#Install Nginx web server (w/ Puppet)
+
+#check if nginx is installed
+package { 'nginx':
+  ensure => 'installed',
+}
+
+#check if nginx is running
+file_line { 'install nginx':
+  ensure => 'present',
+  line => '/etc/nginx/sites-enabled/default',
+  after => 'listen 80 default_server;',
+  line => 'rewrite ^/redirect_me https://www.github.com/laamyry permanent;',
+}
+#redirect_me
+file { '/var/www/html/index.html':
+  content => '<html><body><h1 style="text-align:center">Hello World</h1></body></html>',
+}
+
+#start nginx
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
+}
