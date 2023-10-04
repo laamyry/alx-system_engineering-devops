362af798b845c6739b58c1acf8362f8304e2a3f0
laamyry <slamiri10@gmail.com>
2023-10-04 02:12:27 +0100

    0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,13 @@
 #Install Nginx web server (w/ Puppet)
 
-#check if nginx is installed
-package { 'nginx':
-  ensure => 'installed',
-}
+#Install Nginx web server (w/ Puppet)
 
-#check if nginx is running
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.github.com/laamyry permanent;',
-}
-#redirect_me
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-#start nginx
-service { 'nginx':
-  ensure  => 'running',
-  require => Package['nginx'],
+# do all in one line
+exec {'install nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
+  echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ;
+  sudo sed -i "s/server_name _;/server_name _;
+  \n\trewrite ^\/redirect_me https:\/\/github.com\/laamyry permanent;
+  /" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
