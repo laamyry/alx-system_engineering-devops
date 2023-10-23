8031e75252ae31c4d084fdb6c0996752e8773976
laamyry <slamiri10@gmail.com>
2023-10-24 00:49:59 +0100

    0x15-api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''extend your Python script to export data in the JSON format.'''
+import json
+import requests
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    todo = requests.get(url + "todos").json()
+
+    with open("todo_all_employees.json", "w", newline="") as json_file:
+        json.dump({m.get("id"): [{
+            "task": n.get("title"),
+            "completed": n.get("completed"),
+            "username": m.get("name")
+        } for n in todo if m.get("id") == n.get("userid")]
+            for m in users}, json_file)
