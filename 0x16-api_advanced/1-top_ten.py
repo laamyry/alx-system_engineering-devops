5e50aefa8616c9a0be671c562d9970efea74a884
laamyry <slamiri10@gmail.com>
2023-11-07 23:24:54 +0100

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Top Ten'''
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, headers={"User-Agent": "MyRedditApp/1.0"})
+    if response.status_code != 200:
+        print(None)
+    else:
+        data = response.json()
+        for m in range(10):
+            print(data["data"]["children"][m]['data']['title'])
+        return
